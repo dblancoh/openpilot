cb870363a2456e8447ee3782b578cba7b5d91eff
dblancoh <42918612+dblancoh@users.noreply.github.com>
2021-05-19 13:27:59 +0200

    Show version - 1 more

--- selfdrive/ui/qt/home.cc
+++ selfdrive/ui/qt/home.cc
@@ -83,7 +83,7 @@ OffroadHome::OffroadHome(QWidget* parent) : QFrame(parent) {
   header_layout->addWidget(alert_notification, 0, Qt::AlignHCenter | Qt::AlignRight);
 
   std::string brand = Params().getBool("Passive") ? "dashcam" : "openpilot";
-  QLabel* version = new QLabel(QString::fromStdString(brand + " v" + Params().get("Version").substr(0,5) + Params().get("Version").substr(15,10)));
+  QLabel* version = new QLabel(QString::fromStdString(brand + " v" + Params().get("Version").substr(0,6) + Params().get("Version").substr(15,11)));
   version->setStyleSheet(R"(font-size: 55px;)");
   header_layout->addWidget(version, 0, Qt::AlignHCenter | Qt::AlignRight);
 
