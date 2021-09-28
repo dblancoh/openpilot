9d2c65999db287c2491ac9790352f3a0b42fb332
dblancoh <42918612+dblancoh@users.noreply.github.com>
2021-09-28 17:04:20 +0200

    Offroad translations

--- selfdrive/ui/qt/widgets/drive_stats.cc
+++ selfdrive/ui/qt/widgets/drive_stats.cc
@@ -36,16 +36,16 @@ DriveStats::DriveStats(QWidget* parent) : QFrame(parent) {
     grid_layout->addWidget(labels.distance = newLabel("0", "number"), row, 1, Qt::AlignLeft);
     grid_layout->addWidget(labels.hours = newLabel("0", "number"), row, 2, Qt::AlignLeft);
 
-    grid_layout->addWidget(newLabel("Drives", "unit"), row + 1, 0, Qt::AlignLeft);
+    grid_layout->addWidget(newLabel("Viajes", "unit"), row + 1, 0, Qt::AlignLeft);
     grid_layout->addWidget(labels.distance_unit = newLabel(getDistanceUnit(), "unit"), row + 1, 1, Qt::AlignLeft);
-    grid_layout->addWidget(newLabel("Hours ", "unit"), row + 1, 2, Qt::AlignLeft);
+    grid_layout->addWidget(newLabel("Horas ", "unit"), row + 1, 2, Qt::AlignLeft);
 
     main_layout->addLayout(grid_layout);
   };
 
-  add_stats_layouts("ALL TIME", all_);
+  add_stats_layouts("TODO EL TIEMPO", all_);
   main_layout->addStretch();
-  add_stats_layouts("PAST WEEK", week_);
+  add_stats_layouts("SEMANA PASADA", week_);
 
   if (auto dongleId = getDongleId()) {
     QString url = CommaApi::BASE_URL + "/v1.1/devices/" + *dongleId + "/stats";
